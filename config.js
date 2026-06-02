c99e3e5e8b6730015c1fb088f419485ec1bfaadf
mariyaakter1028-beep <mariyaakter1028@gmail.com>
2026-06-02 20:09:53 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+export const BOT_TOKEN = "8747004115:AAGrMxlH7wl7uToPMjWVKqbVo_btWErqguk";
+
+export const ADMIN_ID = 8136997138;
+
+export const MONGO_URI =
+"mongodb+srv://USERNAME:PASSWORD@cluster.mongodb.net/panelbot";
